7d734e6e3ff20c3cfd373da5f5695e7e6a4aaee9
TiersWar <TiersWar@yandex.ru>
2016-06-09 10:45:19 +0600

    Work function WriteLog

--- main.cpp
+++ main.cpp
@@ -13,25 +13,55 @@ const int MAX_G = 4;
 struct State{
 	char DataState[SZ];
 	State *Generations[MAX_G];
-} nullState;
+} NullState, SourceState, DestinationState;
 
+struct ProgrammStatus{
+	
+	bool dbg;
+	bool IsOn(void){return dbg;}
+	void Switch(void){ dbg = (dbg?false:true);}
+	void Set(bool mode){dbg = mode;}
+	
+	
+}Debug;
+
+std::ostream &TargetLog = std::cout;
 std::list<State>StorageOfStates;
 std::stack<e_Direction>WayFromStartToFinish;
 
 void CalculteAllGenerations(State &ParentState);
-
+void WriteLog(const char *msg, const char* msg2 = NULL);
 bool CheckCollisionOfState(const State &one, const State &two);
 
-
+using std::cout;
+using std::cin;
+using std::endl;
+using std::cerr;
 
 int main(int argc, char **argv){
 	
-	memset(nullState.DataState, -1, SZ);
+	memset(NullState.DataState, -1, SZ);
 	for(int i = 0; i < MAX_G; i++)
-		nullState.Generations[i] = 0;
+		NullState.Generations[i] = 0;
 
+	memset(SourceState.DataState, -1, SZ);
+	for(int i = 0; i < MAX_G; i++)
+		SourceState.Generations[i] = 0;
+		
+	memset(DestinationState.DataState, -1, SZ);
+	for(int i = 0; i < MAX_G; i++)
+		DestinationState.Generations[i] = 0;
 
-
+	Debug.Set(true);
+	
+	WriteLog("Start programm!", "Test run.");
+	WriteLog("Write log 1");
+	WriteLog("Write log 2");
+	Debug.Set(false);
+	WriteLog("Write log 3");
+	Debug.Set(true);
+	WriteLog("Write log 4");
+	
 	return 0;
 }//end main()
 
@@ -46,7 +76,33 @@ void CalculteAllGenerations(State &ParentState){
 
 bool CheckCollisionOfState(const State &one, const State &two){
 	
+	bool result = false;
 	
+	for(int i = 0; i < MAX_G; i++){
+		if(one.DataState[i] == two.DataState[i]){
+			
+			result = true;
+			break;
+		}//if
+		
+	}// for
 	
-	return false;
+	return result;
 }// end CheckCollisionOfState()
+
+
+
+void WriteLog(const char *msg, const char* msg2){
+	
+	if(!Debug.IsOn()){
+		return;
+	}//if
+	
+	TargetLog << msg;
+	if(msg2){
+			TargetLog << " : " << msg2;
+	}//if
+	TargetLog << endl;
+	TargetLog.flush();
+	
+}// end WriteLog()
